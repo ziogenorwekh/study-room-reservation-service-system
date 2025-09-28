156cae98957dc8cbb4ee0793319ce1a806bb8444
ansml <ansml@shojo65-andes.nord>
2025-09-28 22:30:08 +0900

    [fix] exception

--- src/main/java/com/choongang/studyreservesystem/exception/StudyReserveSytemException.java
+++ src/main/java/com/choongang/studyreservesystem/exception/StudyReserveSytemException.java
@@ -7,6 +7,14 @@ public class StudyReserveSytemException extends Exception {
 	 */
 	private static final long serialVersionUID = 3000L;
 
+	public StudyReserveSytemException() {
+		super();
+	}
+
+	public StudyReserveSytemException(String message) {
+		super(message);
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
--- src/main/java/com/choongang/studyreservesystem/exception/UserDuplicationException.java
+++ src/main/java/com/choongang/studyreservesystem/exception/UserDuplicationException.java
@@ -6,9 +6,13 @@ public class UserDuplicationException extends StudyReserveSytemException {
 	 * 
 	 */
 	private static final long serialVersionUID = 3100L;
+	
+	public UserDuplicationException() {
+		super();
+	}
 
-	public UserDuplicationException(String string) {
-		
+	public UserDuplicationException(String message) {
+		super(message);
 	}
 
 	public static long getSerialversionuid() {
